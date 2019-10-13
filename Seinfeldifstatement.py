f390ac816799ea9aea0dfcf0514f68e322f977ca
Ninajyp <ninajyposada@gmail.com>
2019-10-12 21:48:03 -0500

    first commit

---
+++ Seinfeldifstatement.py
@@ -0,0 +1,16 @@
+seinfeldCharacter = input("Enter your favorite seinfeld character ")
+
+if seinfeldCharacter == "Kramer":
+    print("Hey Buddy")
+
+elif seinfeldCharacter == "Elaine":
+    print("I hate men but im not a lesbian!")
+
+elif seinfeldCharacter == "George":
+    print("I was in the pool!")
+
+elif seinfeldCharacter == "Jerry":
+    print("Thats a shame.")
+
+else:
+    print("Look, you're a nice guy but I actually only have three freinds, I cant handle more.")
